8a06d9c94706b52cb065f714861d271a43eba6c2
eekals <96457485+eekals@users.noreply.github.com>
2025-03-19 22:47:07 +0900

    블로그리스트수정

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
-    company: "weniv",
-    position: "CEO",
+    username: "DaminO",
+    company: "Oh",
+    position: "main",
     img: "img/user/profile-licat.png",
   },
 ];
